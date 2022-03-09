15de3dd0006d1998b35b274bd0c1cbba99b97c94
bhargavabhamidipati <bhargavabhamidipati@gmail.com>
2022-03-09 19:14:44 +0100

    Initial commit

---
+++ src/test/java/Testsclass.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Testsclass {
+    @Test
+    public void testAdd(){
+        Calculator obj;
+        obj = new Calculator();
+        assertEquals(4,obj.add(2,2));
+    }
+
+}
